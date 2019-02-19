7095de9b088218d45ba086c5794f2393154171a6
Jason Chu <jasonchu721@gmail.com>
2019-02-18 17:23:35 -0700

    Add colorize gem

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,50 @@
+class NotesController < ApplicationController
+  def index
+    @notes = Note.all
+  end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note = Note.find(params[:id])
+
+    if @note.update(notes_params)
+      redirect_to notes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Note.find(params[:id]).destroy
+    redirect_to notes_path
+  end
+
+  def create 
+    @note = Note.new(notes_params)
+
+    if @note.save
+      redirect_to notes_path
+    else
+      render :mew
+    end
+  end
+
+  private
+
+    def notes_params
+      params.require(:note).permit(:title, :author, :body)
+    end
+
+end
+
